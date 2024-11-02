0f2539578a9814cf66c40e70884b9ce3d2eb0b8d
adina <adina.georgea@stud.ubbcluj.ro>
2024-11-02 13:36:12 +0200

    Rezolvare tema

--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Georgea_Adina_Lab2.Data;
 using Georgea_Adina_Lab2.Models;
+using Georgea_Adina_Lab2.Models.ViewModels;
 
 namespace Georgea_Adina_Lab2.Pages.Categories
 {
@@ -19,11 +20,35 @@ namespace Georgea_Adina_Lab2.Pages.Categories
             _context = context;
         }
 
-        public IList<Category> Category { get;set; } = default!;
+        public IList<Category> Category { get; set; } = default!;
+        public CategoryIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            Category = await _context.Category.ToListAsync();
+            CategoryData = new CategoryIndexData();
+
+            Category = await _context.Category
+                 .Include(c => c.BookCategories)
+                 .ThenInclude(bc => bc.Book)
+                 .ThenInclude(b => b.Author)
+                 .OrderBy(c => c.CategoryName)
+                 .ToListAsync();
+
+            CategoryData.Categories = Category;
+
+            if (id != null)
+            {
+                CategoryID = id.Value;
+                Category category = Category
+                    .Where(c => c.ID == id.Value).Single();
+
+                if (category != null)
+                {
+                    CategoryData.Books = category.BookCategories.Select(bc => bc.Book).ToList();
+                }
+            }
         }
     }
 }
